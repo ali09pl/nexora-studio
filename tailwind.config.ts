ee93a2820ce8249ce1dce8151e70c568e187b6e6
ali09pl <narotootchamy@gmail.com>
2026-01-31 00:31:10 +0100

    clean build setup

--- tailwind.config.ts
+++
@@ -1,7 +0,0 @@
-import type { Config } from "tailwindcss"
-
-export default {
-  content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
-  theme: { extend: {} },
-  plugins: []
-} satisfies Config
