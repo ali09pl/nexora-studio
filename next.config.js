30444eb7a5f9a4f406442e57dad485e0df21529d
ali09pl <narotootchamy@gmail.com>
2026-01-31 00:20:46 +0100

    fix next config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    // عطّل turbopack
-    turbo: false
-  }
+  reactStrictMode: true
 }
 
 module.exports = nextConfig
