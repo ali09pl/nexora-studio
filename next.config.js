9467fa3156f19df3f9fbc2cadccc2d15ecc160d0
ali09pl <narotootchamy@gmail.com>
2026-01-31 01:01:49 +0100

    Setup for Vercel build

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  swcMinify: true, // يستعمل SWC WASM على Vercel
+  reactStrictMode: true,
+}
 
 module.exports = nextConfig
